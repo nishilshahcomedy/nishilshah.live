9c9ac03a9ec62f89dfdc580d1ba3d3def02f1ea7
nishilshahcomedy <nishilshahcomedy@gmail.com>
2026-08-27 14:53:06 -0400

    on oct 25 slug

--- shows.js
+++ shows.js
@@ -39,6 +39,7 @@ window.SHOWS = [
   { date: "2026-08-30", venue: "The Comic Strip", city: "New York, NY" },
   { date: "2026-08-30", venue: "Local NY", city: "Queens, NY" },
   { date: "2026-09-12", venue: "Rodney's Comedy Club", city: "New York, NY", url: "https://www.eventbrite.com/e/9th-annual-comedy-mob-festival-tickets-1997980769265?aff=oddtdtcreator&keep_tld=true" },
-  { date: "2026-09-23", venue: "Whiskey Cellar", city: "New York, NY" },
+  { date: "2026-09-09", venue: "Whiskey Cellar", city: "New York, NY" },
   { date: "2026-09-26", venue: "Flavor Cult", city: "Pond Eddy, NY", note: "$10 in person" },
+  { date: "2026-10-25", venue: "Nishil Shah + Friends", city: "New York, NY", url: "/on-oct-25", mine: true, featured: true },
 ];
